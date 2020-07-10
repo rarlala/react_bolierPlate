a5fc0e98fcfc0d90d2c9aae4807322fff8d2e995
JEONGSUJI <wtw9611@gmail.com>
2020-07-10 11:38:22 +0900

    feat: MongoDB connection (mongoose, Schema)

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const mongoose = require('mongoose');
+mongoose
+  .connect('mongodb+srv://dbUser:dbUser1234@react-basic.q69yi.mongodb.net/dbUser?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
 app.get('/', (req, res) => res.send('hi'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));

6c4ac9b1bd602aea2a6634b768aaad4ec733c96e
JEONGSUJI <wtw9611@gmail.com>
2020-07-14 23:58:47 +0900

    feat: sign up

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.get('/api/hello', (req, res) => {
   res.send('안녕하세요~');
 });
 
-app.post('/api/users/registers', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   // 회원 가입 시 필요한 정보들을 client에서 가져오려면 정보를 DB에 넣어준다
   const user = new User(req.body);
 
@@ -42,7 +42,7 @@ app.post('/api/users/registers', (req, res) => {
   });
 });
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
   // 요청된 이메일을 DB에 있는지 찾기
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -51,18 +51,18 @@ app.post('/login', (req, res) => {
         message: '제공된 이메일에 해당하는 유저가 없습니다.',
       });
     }
-  });
 
-  user.comparePassword(req.body.password, (err, isMatch) => {
-    // 요청한 이메일이 DB에 있다면, 비밀번호가 일치하는지 확인
-    if (!isMatch) return res.json({ loginSuccess: false, message: '비밀번호가 틀렸습니다.' });
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      // 요청한 이메일이 DB에 있다면, 비밀번호가 일치하는지 확인
+      if (!isMatch) return res.json({ loginSuccess: false, message: '비밀번호가 틀렸습니다.' });
 
-    // 비밀번호까지 같다면 유저를 위한 token을 생성하기
-    user.generateToken((err, user) => {
-      if (err) return res.status(400).send(err);
+      // 비밀번호까지 같다면 유저를 위한 token을 생성하기
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
 
-      // 토큰을 저장한다.(쿠키, 로컬스토리지, 세션)
-      res.cookie('x_auth', user.token).status(200).json({ loginSuccess: true, userId: user._id });
+        // 토큰을 저장한다.(쿠키, 로컬스토리지, 세션)
+        res.cookie('x_auth', user.token).status(200).json({ loginSuccess: true, userId: user._id });
+      });
     });
   });
 });
